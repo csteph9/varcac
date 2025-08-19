2cb5faf208610175abdaa822c18ccb5a398b5722
csteph9 <csteph9@gmail.com>
2025-08-18 22:59:19 -0700

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,3 +1,9 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ * Copyright (c) 2025 Caleb Stephens (csteph9@gmail.com)
+ * See the LICENSE file in the project root for license information.
+ */
+
 import 'dotenv/config'
 import mysql from 'mysql2/promise'
 
